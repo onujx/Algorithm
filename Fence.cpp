8854a93ab323dc2c0854bfcc0d9058ea8a037a97
bbd <zxczv41@gmail.com>
2021-01-11 17:43:29 +0900

    Add files via upload

---
+++ Fence.cpp
@@ -0,0 +1,32 @@
+#include<numeric>
+#include<algorithm>
+#include<cassert>
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+
+vector<int> h;
+
+int solve(int left, int right) {
+	if (left == right) { return h[left]; }
+	int mid = (left + right);
+	int ret = max(solve(left, mid), solve(mid + 1, right));
+	int lo = mid, hi = mid + 1;
+	int height = min(h[lo], h[hi]);
+
+	ret = max(ret, height * 2);
+	while (left < lo || hi < right) {
+		if (hi < right && (lo == left || h[lo - 1] < h[hi + 1])) {
+			++hi;
+			height = min(height, h[hi]);
+		}
+		else {
+			--lo;
+			height = min(height, h[lo]);
+		}
+		ret = max(ret, height * (hi - lo + 1));
+	}
+	return ret;
+}
